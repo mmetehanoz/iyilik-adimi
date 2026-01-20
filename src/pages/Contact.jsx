7c21f8e97e8242582e7b464d8fb58e8c38de0990
mmetehanoz <babilyazilim@gmail.com>
2026-01-20 13:36:25 +0300

    Update Google Maps embed code

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -88,7 +88,7 @@ export default function Contact() {
                         {/* Map Embed */}
                         <div className="h-64 rounded-xl overflow-hidden shadow-sm border border-gray-100">
                             <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3009.610264627038!2d29.1729052!3d41.0166014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cabbd2f5a6a6a3%3A0x6b0a0a0a0a0a0a0a!2sYukar%C4%B1%20Dudullu%2C%20Katibim%20Sk.%20No%3A1%2C%2034775%20%C3%9Cmraniye%2F%C4%B0stanbul!5e0!3m2!1str!2str!4v1710000000000!5m2!1str!2str"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3010.41141255489!2d29.151476612384766!3d41.01625427123005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cacf58ef01d7d9%3A0x2e559363c44840e7!2zxLB5aWxpayBBZMSxbcSxIFVsdXNsYXJhcmFzxLEgxLBuc2FuaSBZYXJkxLFtIERlcm5lxJ9p!5e0!3m2!1str!2str!4v1768905337506!5m2!1str!2str"
                                 width="100%"
                                 height="100%"
                                 style={{ border: 0 }}
